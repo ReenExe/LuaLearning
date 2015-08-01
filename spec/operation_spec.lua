f995d857d7d1aea5cddc34e04db7e59a6c05d11d
ReenExe <ipourri@gmail.com>
2015-08-02 02:14:22 +0300

    progress

--- spec/operation_spec.lua
+++ spec/operation_spec.lua
@@ -6,6 +6,11 @@ describe("operation", function()
 		assert.same(5 / 5, 1)
 		assert.same(5 % 3, 2)
 
+		assert.same(1e1, 10)
+		assert.same(1E1, 10)
+		assert.same(-1e-1, -0.1)
+		assert.same(0xF, 15)
+		assert.same(0XF, 15)
 	end)
 
 	it("compare", function()
