12c5ff1d5b6ca60e463ca0a5abaf132a385a5c7f
ReenExe <ipourri@gmail.com>
2015-08-02 02:02:15 +0300

    compare

--- spec/operation_spec.lua
+++ spec/operation_spec.lua
@@ -1,10 +1,14 @@
 describe("operation", function()
-	it("opearion", function() 
+	it("math", function() 
 		assert.same(1 + 1, 2)
 		assert.same(3 - 2, 1)
 		assert.same(5 * 5, 25)
 		assert.same(5 / 5, 1)
 		assert.same(5 % 3, 2)
-		
+
+	end)
+
+	it("compare", function()
+		assert.True(1 == 1)
 	end)
 end)
