e0bca89a0e3fa50ed14c250a553effa73e2725ab
ReenExe <ipourri@gmail.com>
2015-08-02 01:55:40 +0300

    simple operations

---
+++ spec/operation_spec.lua
@@ -0,0 +1,10 @@
+describe("operation", function()
+	it("opearion", function() 
+		assert.same(1 + 1, 2)
+		assert.same(3 - 2, 1)
+		assert.same(5 * 5, 25)
+		assert.same(5 / 5, 1)
+		assert.same(5 % 3, 2)
+		
+	end)
+end)
