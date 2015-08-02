8e16c0a4e8e25e1f2069af84b51dcbeceea22804
ReenExe <ipourri@gmail.com>
2015-08-03 00:45:00 +0300

    progress

--- spec/string_spec.lua
+++ spec/string_spec.lua
@@ -4,4 +4,16 @@ describe("string", function()
         assert.same(string.gsub(first, 'first', 'second'), 'second string')
         assert.same(first, 'first string')
     end)
+  
+    it('length', function()
+        assert.same(#'some value', 10)
+        local value = 'value';
+        assert.same(#value, 5)
+    end)
+    
+    it('long', function()
+        local value = [[
+            <html></html>
+        ]]
+    end)
 end)
