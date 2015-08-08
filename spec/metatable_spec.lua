3d9b000eebb34c041af10891ef6ce3d08278523c
ReenExe <ipourri@gmail.com>
2015-08-08 22:37:57 +0300

    progress

--- spec/metatable_spec.lua
+++ spec/metatable_spec.lua
@@ -1,15 +1,21 @@
 describe("metatable", function()
+  it('get and set', function()
+    local table = {}
+
+    assert.True(getmetatable(table) == nil)
+  end)
+
 	it("simple tostring", function()
     local metatable = {
       __tostring = function()
         return 'value'
       end
     }
-      
+
 		local table = {}
-    
+
     setmetatable(table, metatable)
-    
+
     assert.True(tostring(table) == 'value');
 	end)
 end)
