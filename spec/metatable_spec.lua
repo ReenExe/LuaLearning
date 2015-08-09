25827c114dda9978572e63638ab9fbf22585ed6d
ReenExe <ipourri@gmail.com>
2015-08-09 15:27:58 +0300

    test extend

--- spec/metatable_spec.lua
+++ spec/metatable_spec.lua
@@ -11,7 +11,7 @@ describe("metatable", function()
     assert.True(getmetatable(table) == metatable)
   end)
 
-	it('simple tostring', function()
+	it('simple __tostring', function()
     local metatable = {
       __tostring = function()
         return 'value'
@@ -23,7 +23,7 @@ describe("metatable", function()
     setmetatable(table, metatable)
 
     assert.True(tostring(table) == 'value');
-    
+
     assert.True(table.value == nil)
     table[table] = true
     assert.True(table.value == nil)
@@ -36,85 +36,85 @@ describe("metatable", function()
     mt.__tostring = function()
       return 'value'
     end
-    
+
     assert.False(tostring(table) == 'value');
   end)
 
   it('compare <', function()
-      local mt = {
-        __lt = function(a, b)
-          return a.value < b.value
-        end
-      }
+    local mt = {
+      __lt = function(a, b)
+        return a.value < b.value
+      end
+    }
 
-      local a = {}
-      local b = {}
+    local a = {}
+    local b = {}
 
-      setmetatable(a, mt)
-      setmetatable(b, mt)
+    setmetatable(a, mt)
+    setmetatable(b, mt)
 
-      a.value = 1
-      b.value = 2
+    a.value = 1
+    b.value = 2
 
-      assert.True(a < b)
-      assert.True(b > a)
-      assert.False(a > b)
-      assert.False(b < a)
+    assert.True(a < b)
+    assert.True(b > a)
+    assert.False(a > b)
+    assert.False(b < a)
   end)
 
   it('compare <=', function()
-      local mt = {
-        __le = function(a, b)
-          return a.value <= b.value
-        end
-      }
+    local mt = {
+      __le = function(a, b)
+        return a.value <= b.value
+      end
+    }
 
-      local a = {}
-      local b = {}
+    local a = {}
+    local b = {}
 
-      setmetatable(a, mt)
-      setmetatable(b, mt)
+    setmetatable(a, mt)
+    setmetatable(b, mt)
 
-      a.value = 1
-      b.value = 2
+    a.value = 1
+    b.value = 2
 
-      assert.True(a <= b)
-      assert.True(b >= a)
-      assert.False(a >= b)
-      assert.False(b <= a)
-      
-      a.value = 1
-      b.value = 1
-      
-      assert.True(a <= b)
-      assert.True(b >= a)
-      assert.True(a >= b)
-      assert.True(b <= a)
+    assert.True(a <= b)
+    assert.True(b >= a)
+    assert.False(a >= b)
+    assert.False(b <= a)
+
+    a.value = 1
+    b.value = 1
+
+    assert.True(a <= b)
+    assert.True(b >= a)
+    assert.True(a >= b)
+    assert.True(b <= a)
   end)
 
   it('compare ==', function()
-      local mt = {
-        __eq = function(a, b)
-          return a.value == b.value
-        end
-      }
+    local mt = {
+      __eq = function(a, b)
+        return a.value == b.value
+      end
+    }
 
-      local a = {}
-      local b = {}
+    local a = {}
+    local b = {}
 
-      setmetatable(a, mt)
-      setmetatable(b, mt)
+    setmetatable(a, mt)
+    setmetatable(b, mt)
 
-      a.value = 1
-      b.value = 2
+    a.value = 1
+    b.value = 2
 
-      assert.False(a == b)
-      assert.True(b ~= a)
-      
-      a.value = 1
-      b.value = 1
-      
-      assert.True(a == b)
+    assert.False(a == b)
+    assert.True(b ~= a)
+
+    a.value = 1
+    b.value = 1
+
+    assert.True(a == b)
   end)
 
   it('compare all', function()
@@ -135,19 +135,19 @@ describe("metatable", function()
 
     setmetatable(a, mt)
     setmetatable(b, mt)
-    
+
     a.value = 7
     b.value = 10
     assert.True(a == b)
-    
+
     a.value = 1;
     a.multi = 1;
     b.value = 3;
     b.multi = 3;
-    
+
     assert.True(a < b)
     assert.True(b > a)
-    
+
     a.value = 2
     b.value = 5
     a.multi = 1
@@ -155,37 +155,95 @@ describe("metatable", function()
     assert.True(a == b)
     assert.False(a < b)
     assert.True(a <= b)
-    
+
     mt.__eq = function() return false end
     mt.__le = function() return true end
     mt.__lt = function() return true end
-    
+
     assert.False(a == b)
     assert.True(a <= b)
     -- how about that?
     assert.True(a < b and a > b)
   end)
 
-  it('index function', function()
-    local t = {};
-    local mt = {};
-    
+  it('__index function', function()
+    local t = {}
+    local mt = {}
+
     setmetatable(t, mt)
-    
+
     assert.True(t[1] == nil)
 
+    -- AFTER SET!
     mt.__index = function(_, key)
-    
+
       assert.True(_ == t)
-    
+
       return tonumber(key) * 2
     end
-    
+
     assert.True(t[1] == 2)
     assert.True(t[5] == 10)
-    
+
     t[1] = 8
     assert.True(t[1] == 8)
     assert.True(t[5] == 10)
+  end)
+
+  it('__index table', function()
+    local t = {}
+    local mt = {}
+
+    setmetatable(t, mt)
+    mt.__index = mt;
+
+    assert.True(t.value == nil)
+
+    mt.value = 'mt'
+    assert.True(t.value == 'mt')
+
+    t.value = 't'
+    assert.True(t.value == 't')
+    
+    t.value = nil
+    assert.True(t.value == 'mt')
+
+    local index = {}
+    mt.__index = index
+    assert.True(t.value == nil)
+
+    index.value = 'index'
+    assert.True(t.value == 'index')
+  end)
+
+  it('__index extend', function()
+    local t = {}
+    local mt = {}
+    local mtparent = {}
+
+    setmetatable(mt, mtparent)
+    setmetatable(t, mt)
+
+    assert.True(t.value == nil)
+    assert.True(mt.value == nil)
+    assert.True(mtparent.value == nil)
+
+    mt.__index = mt
+    mtparent.__index = mtparent
+
+    mtparent.value = 'mtparent'
+    assert.True(mtparent.value == 'mtparent')
+    assert.True(mt.value == 'mtparent')
+    assert.True(t.value == 'mtparent')
+
+    mt.value = 'mt'
+    assert.True(mtparent.value == 'mtparent')
+    assert.True(mt.value == 'mt')
+    assert.True(t.value == 'mt')
+
+    t.value = 't'
+    assert.True(mtparent.value == 'mtparent')
+    assert.True(mt.value == 'mt')
+    assert.True(t.value == 't')
   end)
 end)
